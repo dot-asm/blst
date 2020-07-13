773f547f92957a71084943bcc786899aeec170cc
Andy Polyakov <appro@cryptogams.org>
2020-07-13 11:19:04 +0200

    bindings/python/demo.py: switch to classes.

--- bindings/python/demo.py
+++ bindings/python/demo.py
@@ -5,33 +5,37 @@ import sys
 import subprocess
 import re
 
+def newer(*files) :
+    assert len(files) > 1
+    rh = files[-1]
+    if not os.path.exists(rh) :
+        return True
+    for lh in files[:-1] :
+        if os.stat(lh).st_ctime > os.stat(rh).st_ctime :
+            return True
+    return False
+
 here = re.split(r'/(?=[^/]*$)', sys.argv[0])
 if len(here) > 1:
     os.chdir(here[0])
 
 if not os.path.exists("libblst.a") :
-    print("building libbst.a...")
+    print("building libblst.a...")
     subprocess.call(["../../build.sh"]) == 0 or sys.exit(1)
 
-def newer(lh, rh) :
-    if os.path.exists(rh) :
-        return os.stat(lh).st_ctime > os.stat(rh).st_ctime
-    else :
-        return True
-
-if newer("../blst.swg", "blst_wrap.cpp") :
+if newer("../blst.swg", "../blst.h", "../blst.hpp", "blst_wrap.cpp") :
     print("swig-ing...")
     subprocess.call(["swig", "-c++", "-python", "-O",
                      "-o", "blst_wrap.cpp", "-outdir", ".",
                      "../blst.swg"]) == 0 or sys.exit(1)
 
-if newer("blst_wrap.cpp", "_blst.so") or newer("libblst.a", "_blst.so") :
+if newer("blst_wrap.cpp", "libblst.a", "_blst.so") :
     print("compiling _blst.so...")
-    pyver = "python{}.{}".format(sys.version_info.major, sys.version_info.minor)
-    subprocess.call(["c++", "-shared", "-o", "_blst.so", "-fPIC",
-                     "-I/usr/include/" + pyver, "-I..", "-O", "-Wall",
-                     "blst_wrap.cpp", "libblst.a", "-Wl,-Bsymbolic"]) == 0 or \
-    sys.exit(1)
+    maj_min = "{}.{}".format(sys.version_info.major, sys.version_info.minor)
+    subprocess.call(["c++", "-shared", "-o", "_blst.so", "-fPIC", #"-std=c++17",
+                     "-I/usr/include/python" + maj_min, "-I..", "-O", "-Wall",
+                     "blst_wrap.cpp", "libblst.a", "-Wl,-Bsymbolic"]) == 0 \
+    or sys.exit(1)
 
 print("testing...")
 ########################################################################
@@ -39,71 +43,75 @@ import blst
 
 msg = b"assertion"		# this what we're signing
 DST = b"MY-DST"			# domain separation tag
-SK = blst.keygen(b"passw@rd")	# secret key
+
+SK = blst.SecretKey()
+SK.keygen(b"passw@rd")		# secret key
 
 ########################################################################
 # generate public key and signature
 
-pk_for_wire = blst.p1_serialize(blst.sk_to_pk_in_g1(SK))
-				#vvvvvvvvvvv optional augmentation
-hash = blst.hash_to_g2(msg, DST, pk_for_wire)
-sig_for_wire = blst.p2_serialize(blst.sign_pk_in_g1(hash, SK))
+pk = blst.P1(SK)
+pk_for_wire = pk.serialize()
+
+sig = blst.P2()			#    vvvvvvvvvvv optional augmentation
+sig_for_wire = sig.hash_to(msg, DST, pk_for_wire) \
+                  .sign_with(SK) \
+                  .serialize()
 
 ########################################################################
 # at this point 'pk_for_wire', 'sig_for_wire' and 'msg' are
 # "sent over network," so now on "receiver" side
 
-_, pk = blst.p1_deserialize(pk_for_wire)
-_, sig = blst.p2_deserialize(sig_for_wire)
-if blst.p1_affine_in_g1(pk) :	# vet the public key
-    ctx = blst.pairing_init()
-    blst.pairing_aggregate_pk_in_g1(ctx, pk, sig,
-                    True,	# because "sender" called hash_to_g2
-                    msg, DST, pk_for_wire)
-    blst.pairing_commit(ctx)
-    result = blst.pairing_finalverify(ctx)
-
-print(result)
+sig = blst.P2_Affine(sig_for_wire)
+pk  = blst.P1_Affine(pk_for_wire)
+if pk.in_group() :		# vet the public key
+    ctx = blst.Pairing()
+    ctx.aggregate(pk, sig,
+                  True,		# because "sender" called hash_to
+                  msg, DST, pk_for_wire)
+    ctx.commit()
+    print(ctx.finalverify())
+else :
+    print("disaster")
 
 ########################################################################
 # generate public key and signature
 
-pk_for_wire = blst.p2_serialize(blst.sk_to_pk_in_g2(SK))
-				#vvvvvvvvvvv optional augmentation
-hash = blst.hash_to_g1(msg, DST, pk_for_wire)
-sig_for_wire = blst.p1_serialize(blst.sign_pk_in_g2(hash, SK))
+pk = blst.P2(SK)
+pk_for_wire = pk.serialize()
+
+sig = blst.P1()			#    vvvvvvvvvvv optional augmentation
+sig_for_wire = sig.hash_to(msg, DST, pk_for_wire) \
+                  .sign_with(SK) \
+                  .serialize()
 
 ########################################################################
 # at this point 'pk_for_wire', 'sig_for_wire' and 'msg' are
 # "sent over network," so now on "receiver" side
 
-_, pk = blst.p2_deserialize(pk_for_wire)
-_, sig = blst.p1_deserialize(sig_for_wire)
-if blst.p2_affine_in_g2(pk) :	# vet the public key
-    ctx = blst.pairing_init()
-    blst.pairing_aggregate_pk_in_g2(ctx, pk, sig,
-                    True,	# because "sender" called hash_to_g2
-                    msg, DST, pk_for_wire)
-    blst.pairing_commit(ctx)
-    result = blst.pairing_finalverify(ctx)
+sig = blst.P1_Affine(sig_for_wire)
+pk  = blst.P2_Affine(pk_for_wire)
+if pk.in_group() :		# vet the public key
+    ctx = blst.Pairing()
+    ctx.aggregate(pk, sig,
+                  True,		# because "sender" called hash_to
+                  msg, DST, pk_for_wire)
+    ctx.commit()
+    print(ctx.finalverify())
+else :
+    print("disaster")
 
-print(result)
+########################################################################
+# from https://github.com/supranational/blst/issues/5
 
 pk_for_wire = bytes.fromhex("ab10fc693d038b73d67279127501a05f0072cbb7147c68650ef6ac4e0a413e5cabd1f35c8711e1f7d9d885bbc3b8eddc")
 sig_for_wire = bytes.fromhex("a44158c08c8c584477770feec2afa24d5a0b0bab2800414cb9efbb37c40339b6318c9349dad8de27ae644376d71232580ff5102c7a8579a6d2627c6e40b0ced737a60c66c7ebd377c04bf5ac957bf05bc8b6b09fbd7bdd2a7fa1090b5a0760bb")
 msg = bytes.fromhex("0000000000000000000000000000000000000000000000000000000000000000")
 DST = bytes.fromhex("424c535f5349475f424c53313233383147325f584d443a5348412d3235365f535357555f524f5f504f505f")
 
-_, pk = blst.p1_uncompress(pk_for_wire)
-_, sig = blst.p2_uncompress(sig_for_wire)
-if blst.p1_affine_in_g1(pk) :	# vet the public key
-    ctx = blst.pairing_init()
-    blst.pairing_aggregate_pk_in_g1(ctx, pk, sig,
-                    True,	# because "sender" called hash_to_g2
-                    msg, DST)
-    blst.pairing_commit(ctx)
-    result = blst.pairing_finalverify(ctx)
-
-print(result)
-
-
+sig = blst.P2_Affine(sig_for_wire)
+pk  = blst.P1_Affine(pk_for_wire)
+if pk.in_group() :	# vet the public key
+    print(sig.core_verify(pk, True, msg, DST) == blst.BLST_SUCCESS)
+else :
+    print("disaster")
