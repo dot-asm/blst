31f65ef2d94df79198ca70793ddb555b1ab2aa31
Andy Polyakov <appro@cryptogams.org>
2022-07-03 19:03:44 +0200

    bindings/emscipten/run.me: add thunks required to run test from node.js.

--- bindings/emscipten/runnable.js
+++ bindings/emscipten/runnable.js
@@ -1,14 +1,40 @@
+'use strict';
+
+console.log("testing...");
+
 const blst = require("blst");
 
 blst['onRuntimeInitialized'] = function() {
-  var a = blst.P2_Affine.generator();
-  var p = new blst.P2_Affine(a.compress());
-  if (!p.is_equal(a)) throw "disaster";
-  var x = p.to_jacobian();
-  var y = new blst.P2.generator();
-  if (!p.is_equal(y.to_affine())) throw "disaster";
-  x.dbl();
-  y.add(p);
-  if (!x.is_equal(y)) throw "disaster";
+  var msg = "assertion";          // this what we're signing
+  var DST = "MY-DST";             // domain separation tag
+
+  var SK = new blst.SecretKey();
+  SK.keygen("*".repeat(32));
+
+  ////////////////////////////////////////////////////////////////////////
+  // generate public key and signature
+
+  var pk = new blst.P1(SK);
+  var pk_for_wire = pk.serialize();
+
+  var sig = new blst.P2();
+  var sig_for_wire = sig.hash_to(msg, DST, pk_for_wire)
+                        .sign_with(SK)
+                        .serialize();
+
+  ////////////////////////////////////////////////////////////////////////
+  // at this point 'pk_for_wire', 'sig_for_wire' and 'msg' are
+  // "sent over network," so now on "receiver" side
+
+  sig = new blst.P2_Affine(sig_for_wire);
+  pk  = new blst.P1_Affine(pk_for_wire);
+
+  if (!pk.in_group()) throw "disaster";   // vet the public key
+
+  var ctx = new blst.Pairing(true, DST);
+  ctx.aggregate(pk, sig, msg, pk_for_wire);
+  ctx.commit();
+  if (!ctx.finalverify()) throw "disaster";
+
   console.log("OK");
 }
