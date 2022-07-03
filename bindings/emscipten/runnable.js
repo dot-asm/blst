f2128af99774f3681387f563ac52d583c3b9561d
Andy Polyakov <appro@cryptogams.org>
2022-07-03 14:47:07 +0200

    bindings/emscipten/run.me: add P{12}.

--- bindings/emscipten/runnable.js
+++ bindings/emscipten/runnable.js
@@ -1,6 +1,14 @@
 const blst = require("blst");
 
 blst['onRuntimeInitialized'] = function() {
-  var a = blst.P1_Affine.generator().compress();
-  var p = new blst.P1_Affine(a);
+  var a = blst.P2_Affine.generator();
+  var p = new blst.P2_Affine(a.compress());
+  if (!p.is_equal(a)) throw "disaster";
+  var x = p.to_jacobian();
+  var y = new blst.P2.generator();
+  if (!p.is_equal(y.to_affine())) throw "disaster";
+  x.dbl();
+  y.add(p);
+  if (!x.is_equal(y)) throw "disaster";
+  console.log("OK");
 }
