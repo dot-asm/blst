fa11f56d1285a7e21e355bd50370c6f0cd738f7a
Andy Polyakov <appro@cryptogams.org>
2026-07-05 17:26:35 +0200

    build/assembly.S: take in elf/mula_mont_384-x86_64.s.

--- src/vect.h
+++ src/vect.h
@@ -75,16 +75,29 @@ typedef limb_t bool_t;
 # define sqr_mont_sparse_256 sqrx_mont_sparse_256
 # define from_mont_256 fromx_mont_256
 # define redc_mont_256 redcx_mont_256
-# define mul_mont_384 mulx_mont_384
-# define sqr_mont_384 sqrx_mont_384
-# define sqr_n_mul_mont_384 sqrx_n_mul_mont_384
-# define sqr_n_mul_mont_383 sqrx_n_mul_mont_383
-# define mul_384 mulx_384
-# define sqr_384 sqrx_384
-# define redc_mont_384 redcx_mont_384
-# define from_mont_384 fromx_mont_384
-# define sgn0_pty_mont_384 sgn0x_pty_mont_384
-# define sgn0_pty_mont_384x sgn0x_pty_mont_384x
+# if defined(__APX_F__)
+#  define mul_mont_384 mula_mont_384
+#  define sqr_mont_384 sqra_mont_384
+#  define sqr_n_mul_mont_384 sqra_n_mul_mont_384
+#  define sqr_n_mul_mont_383 sqra_n_mul_mont_383
+#  define mul_384 mula_384
+#  define sqr_384 sqra_384
+#  define redc_mont_384 redca_mont_384
+#  define from_mont_384 froma_mont_384
+#  define sgn0_pty_mont_384 sgn0a_pty_mont_384
+#  define sgn0_pty_mont_384x sgn0a_pty_mont_384x
+# else
+#  define mul_mont_384 mulx_mont_384
+#  define sqr_mont_384 sqrx_mont_384
+#  define sqr_n_mul_mont_384 sqrx_n_mul_mont_384
+#  define sqr_n_mul_mont_383 sqrx_n_mul_mont_383
+#  define mul_384 mulx_384
+#  define sqr_384 sqrx_384
+#  define redc_mont_384 redcx_mont_384
+#  define from_mont_384 fromx_mont_384
+#  define sgn0_pty_mont_384 sgn0x_pty_mont_384
+#  define sgn0_pty_mont_384x sgn0x_pty_mont_384x
+# endif
 # define ct_inverse_mod_384 ctx_inverse_mod_384
 #endif
 
@@ -141,7 +154,13 @@ void ct_inverse_mod_256(vec512 ret, const vec256 inp, const vec256 mod,
                                                       const vec256 modx);
 bool_t ct_is_square_mod_384(const vec384 inp, const vec384 mod);
 
-#if defined(__ADX__) /* e.g. -march=broadwell */ && !defined(__BLST_PORTABLE__)
+#if defined(__APX_F__) /* e.g. -mapxf */ && !defined(__BLST_PORTABLE__)
+# define mul_mont_384x mula_mont_384x
+# define sqr_mont_384x sqra_mont_384x
+# define sqr_mont_382x sqra_mont_382x
+# define mul_382x mula_382x
+# define sqr_382x sqra_382x
+#elif defined(__ADX__) /* e.g. -march=broadwell */ && !defined(__BLST_PORTABLE__)
 # define mul_mont_384x mulx_mont_384x
 # define sqr_mont_384x sqrx_mont_384x
 # define mul_382x mulx_382x
